92762e02d3ec4f31e786024e343c3a847e547fe0
ww <wangwei@xiangha.com>
2019-11-21 16:23:57 +0800

    v2ray

--- v2ray.sh
+++ v2ray.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-yum -y install ntpdate crontab wget
-
-/bin/systemctl restart crond.service
-
-/bin/systemctl enable crond.service
-
-timedatectl set-timezone Asia/Shanghai
-
-ntpdate -u  pool.ntp.org
-
 file = /usr/bin/v2ray/v2ray
 
 if [ ! -f "$file" ]; then
+
+    yum -y install ntpdate crontab wget
+
+    /bin/systemctl restart crond.service
+
+    /bin/systemctl enable crond.service
+
+    timedatectl set-timezone Asia/Shanghai
+
+    ntpdate -u  pool.ntp.org
+
     echo "*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1" >> /var/spool/cron/root
 
     curl -L -s https://install.direct/go.sh | bash
@@ -22,9 +23,10 @@ if [ ! -f "$file" ]; then
     wget -P /etc/v2ray/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/config.json
 
     /bin/systemctl enable v2ray
+    
 fi
 
 
 curl -L -s https://raw.githubusercontent.com/qingfenghuohu/os_init/master/bbr.sh | bash
 
-rebot
+reboot
