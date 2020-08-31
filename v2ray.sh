21d7764f781b41feeaf0e56667f9565b6ebb0b3b
ww <qingfenghuohu@126.com>
2020-08-31 16:54:24 +0800

    init

--- v2ray.sh
+++ v2ray.sh
@@ -8,52 +8,52 @@ file = /usr/bin/v2ray/v2ray
 
 if [ ! -f "$file" ]; then
 
-    yum -y install ntpdate crontab wget
+  yum -y install ntpdate crontab wget
 
-    /bin/systemctl restart crond.service
+  /bin/systemctl restart crond.service
 
-    /bin/systemctl enable crond.service
+  /bin/systemctl enable crond.service
 
-    timedatectl set-timezone Asia/Shanghai
+  timedatectl set-timezone Asia/Shanghai
 
-    ntpdate -u pool.ntp.org
+  ntpdate -u pool.ntp.org
 
-    curl https://raw.githubusercontent.com/qingfenghuohu/os_init/master/install-v2ray.sh|bash
+  curl https://raw.githubusercontent.com/qingfenghuohu/os_init/master/install-v2ray.sh | bash
 
-    curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh|bash
+  curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh | bash
 
-    rm -f /etc/systemd/system/v2ray.service
+  rm -f /etc/systemd/system/v2ray.service
 
-    rm -f /etc/v2ray/config.json
+  rm -f /etc/v2ray/config.json
 
-    wget -P /etc/systemd/system/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/v2ray.service
+  wget -P /etc/systemd/system/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/v2ray.service
 
-    wget -P /etc/v2ray/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/config.json
+  wget -P /etc/v2ray/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/config.json
 
-    systemctl daemon-reload
+  systemctl daemon-reload
 
-    /bin/systemctl enable v2ray
+  /bin/systemctl enable v2ray
 
-    /bin/systemctl restart v2ray
+  /bin/systemctl restart v2ray
 
-    echo "*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1" >> /var/spool/cron/root
+  echo "*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1" >>/var/spool/cron/root
 
-    echo "1,10 * * * * /bin/systemctl restart v2ray > /dev/null 2>&1" >> /var/spool/cron/root
+  echo "1,10 * * * * /bin/systemctl restart v2ray > /dev/null 2>&1" >>/var/spool/cron/root
 
-    echo "v2ray install success"
+  echo "v2ray install success"
 
 fi
 
-bbr=`lsmod|grep bbr|awk '{print $1}'`
+bbr=$(lsmod | grep bbr | awk '{print $1}')
 
 if [[ "tcp_bbr" != $bbr ]]; then
 
-    bbr = `lsmod|grep bbr|awk '{print $1}'`
+  bbr = $(lsmod | grep bbr | awk '{print $1}'
 
-    curl -L -s https://raw.githubusercontent.com/qingfenghuohu/os_init/master/bbr.sh | bash
+  curl -L -s https://raw.githubusercontent.com/qingfenghuohu/os_init/master/bbr.sh | bash
 
-    reboot
+  reboot
 
-    echo "bbr install success"
+  echo "bbr install success"
 
 fi
