3a540794e20f06be1eea0ab11b7056a862b16a56
ww <qingfenghuohu@126.com>
2020-09-01 12:17:26 +0800

    init

--- install-redis.sh
+++ install-redis.sh
@@ -5,6 +5,7 @@ tar -xzvf redis-5.0.7.tar.gz
 cd redis-5.0.7
 make MALLOC=libc
 make install
+cp redis.conf /usr/local/redis/redis.conf
 wget -P /etc/systemd/system/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/redis.service
 systemctl daemon-reload
 pass = $(head -c 100 /dev/urandom | tr -dc a-z0-9A-Z | head -c 16)
