382fbd7a885bbf3a904f0bb170a7f8626a701141
ww <qingfenghuohu@126.com>
2020-09-01 12:27:06 +0800

    init

--- install-redis.sh
+++ install-redis.sh
@@ -10,8 +10,8 @@ make install
 cp redis.conf /usr/local/redis/redis.conf
 wget -P /etc/systemd/system/ https://raw.githubusercontent.com/qingfenghuohu/os_init/master/redis.service
 systemctl daemon-reload
-pass = $(head -c 100 /dev/urandom | tr -dc a-z0-9A-Z | head -c 16)
-echo "requirepass $pass" >> /usr/local/redis/redis.conf
+pass=$(head -c 100 /dev/urandom | tr -dc a-z0-9A-Z | head -c 16)
+echo "requirepass $pass" >>/usr/local/redis/redis.conf
 /bin/systemctl enable redis
 /bin/systemctl restart redis
 echo $pass
